f911db80c08cc08c8f37ee01755e4d460ba96931
EmiGHino <emighino123@gmail.com>
2022-01-13 18:26:46 -0600

    Se resuelven tracucciones entre Django y Vuejs

--- omnileads_ui/supervision/src/main.js
+++ omnileads_ui/supervision/src/main.js
@@ -13,11 +13,35 @@ import 'primeicons/primeicons.css'                           //icons
 import Card from 'primevue/card';
 import Button from 'primevue/button';
 
+// Idiomas
+import { createI18n } from 'vue-i18n'
+import es from './../i18n/es'
+import en from './../i18n/en'
+import fa from './../i18n/fa'
+import pt_br from '../i18n/pt-br'
+
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+const lenguage = cookies.get("django_language")
+
+// Configuramos los idiomas
+const i18n = createI18n({
+    locale: lenguage,
+    fallbackLocale: lenguage,
+    messages: {
+        es,
+        en,
+        fa,
+        'pt-br': pt_br
+    }
+})
+
 const app = createApp(App)
 
 app.component('Card', Card)
 app.component('Button', Button)
 
-app.use(router)
+app.use(i18n)
+    .use(router)
     .use(PrimeVue)
     .mount('#app')
